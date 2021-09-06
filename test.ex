d1d8dabf2a36837a604fa2dadb2f1e439a4c0eb2
caseyriley <innerforest7@gmail.com>
2021-09-06 10:13:37 -0700

    add function and recursive function

--- test.ex
+++ test.ex
@@ -1,1 +1,27 @@
-IO.puts "Hello World"
+IO.puts "Hello World" ; IO.puts "Bye"
+defmodule Greeter do
+  def hello(name, country \\ "en") do
+     phrase(country) <> name
+  end
+
+  defp phrase("en"), do: "Hello, "
+  defp phrase("es"), do: "Hola, "
+end
+
+Greeter.hello("Ayush", "en")
+Greeter.hello("Ayush")
+Greeter.hello("Ayush", "es")
+
+
+#recursive function for printing each item in a list
+a = ["Hey", 100, 452, :true, "People"]
+defmodule ListPrint do
+   def print([]) do
+   end
+   def print([head | tail]) do
+      IO.puts(head)
+      print(tail)
+   end
+end
+
+ListPrint.print(a)
