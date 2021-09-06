ba8ce726f2577c7907ef76b76e8d7d7c7aaf97d6
caseyriley <innerforest7@gmail.com>
2021-09-06 10:31:26 -0700

    add recursive loop model

--- test.ex
+++ test.ex
@@ -25,3 +25,17 @@ defmodule ListPrint do
 end
 
 ListPrint.print(a)
+
+#looping with recursion
+defmodule Loop do
+  def print_multiple_times(msg, n) when n <= 1 do
+     IO.puts msg
+  end
+
+  def print_multiple_times(msg, n) do
+     IO.puts msg
+     print_multiple_times(msg, n - 1)
+  end
+end
+
+Loop.print_multiple_times("Hello", 10)
