f19e9ce53335684cd1d8007b6eb1740b66ce94d8
caseyriley <innerforest7@gmail.com>
2021-09-07 12:17:54 -0700

    add protocols

--- test.ex
+++ test.ex
@@ -40,6 +40,55 @@ end
 
 Loop.print_multiple_times("Hello", 10)
 
-#Enum.all?
+#Enum.all? #check if all are odd
 res = Enum.all?([1, 2, 3, 4], fn(s) -> rem(s,2) == 1 end)
 IO.puts(res)
+
+#Enum.any? #check if any are odd
+res = Enum.any?([1, 2, 3, 4], fn(s) -> rem(s,2) == 1 end)
+IO.puts(res)
+
+#iterate over a collection without producing a new value
+Enum.each(["Hello", "Every", "one"], fn(freaky) -> IO.puts(freaky) end)
+
+#apply our function to each item and produce a new collection
+res = Enum.map([2, 5, 3, 6], fn(a) -> a*2 end)
+IO.puts(res)
+
+#reduce our enumerable to a single value #5 is optional accumulator
+res = Enum.reduce([1, 2, 3, 4], 5, fn(x, accum) -> x + accum end)
+IO.puts(res)
+
+#a pipeline of operations
+odd? = &(odd? = &(rem(&1, 2) != 0)
+res = 1..100_000 |> Enum.map(&(&1 * 3)) |> Enum.filter(odd?) |> Enum.sum
+IO.puts(res)
+
+
+#Understanding Protocols
+#Defining the protocol
+defprotocol Blank do
+  def blank?(data)
+end
+
+#Implementing the protocol for lists
+defimpl Blank, for: List do
+  def blank?([]), do: true
+  def blank?(_), do: false
+end
+
+#Implementing the protocol for strings
+defimpl Blank, for: BitString do
+  def blank?(""), do: true
+  def blank?(_), do: false
+end
+
+#Implementing the protocol for maps
+defimpl Blank, for: Map do
+  def blank?(map), do: map_size(map) == 0
+end
+
+IO.puts(Blank.blank? [])
+IO.puts(Blank.blank? [:true, "Hello"])
+IO.puts(Blank.blank? "")
+IO.puts(Blank.blank? "Hi")
