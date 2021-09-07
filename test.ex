5d78c0fc82eae0de89356b11c4ed480906d69bac
caseyriley <innerforest7@gmail.com>
2021-09-07 16:52:12 -0700

    add function specifications

--- test.ex
+++ test.ex
@@ -60,9 +60,9 @@ res = Enum.reduce([1, 2, 3, 4], 5, fn(x, accum) -> x + accum end)
 IO.puts(res)
 
 #a pipeline of operations
-odd? = &(odd? = &(rem(&1, 2) != 0)
-res = 1..100_000 |> Enum.map(&(&1 * 3)) |> Enum.filter(odd?) |> Enum.sum
-IO.puts(res)
+# odd? = &(odd? = &(rem(&1, 2) != 0)
+# res = 1..100_000 |> Enum.map(&(&1 * 3)) |> Enum.filter(odd?) |> Enum.sum
+# IO.puts(res)
 
 
 #Understanding Protocols
@@ -88,7 +88,16 @@ defimpl Blank, for: Map do
   def blank?(map), do: map_size(map) == 0
 end
 
-IO.puts(Blank.blank? [])
-IO.puts(Blank.blank? [:true, "Hello"])
-IO.puts(Blank.blank? "")
-IO.puts(Blank.blank? "Hi")
+defmodule FunnyCalculator do
+  @type number_with_joke :: {number, String.t}
+
+  @spec add(number, number) :: number_with_joke
+  def add(x, y), do: {x + y, "You need a calculator to do that?"}
+
+  @spec multiply(number, number) :: number_with_joke
+  def multiply(x, y), do: {x * y, "It is like addition on steroids."}
+end
+
+{result, comment} = FunnyCalculator.add(10, 20)
+IO.puts(result)
+IO.puts(comment)
